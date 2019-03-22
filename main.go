272ce73f30cf6413c31e415c09c8e591a9ebcfa4
Francis Guimond <fguimond@homeaway.com>
2019-03-22 16:05:59 -0600

    Argument reading and parsing

--- main.go
+++ main.go
@@ -6,11 +6,21 @@ import (
 	"github.com/sensu/sensu-enterprise-go-plugin/sensu"
 	"github.com/sensu/sensu-go/types"
 	"log"
+	"strings"
 )
 
 type Config struct {
 	sensu.HandlerConfig
-	param1 string
+	AwsAccessKeyId        string
+	AwsSecretKey          string
+	AwsRegion             string
+	AwsAccounts           string
+	AllowedInstanceStates string
+	Timeout               uint64
+
+	// Computed from the input
+	AwsAccountsMap           map[string]bool
+	AllowedInstanceStatesMap map[string]bool
 }
 
 var (
@@ -25,14 +35,68 @@ var (
 
 	options = []*sensu.HandlerConfigOption{
 		{
-			Path:      "param1",
-			Env:       "HANDLER_PARAM1",
-			Argument:  "param1",
-			Shorthand: "p",
-			Default:   "defaultvalue",
-			Usage:     "The param1 usage information",
-			Value:     &config.param1,
+			Path:      "aws-access-key-id",
+			Env:       "AWS_ACCESS_KEY_ID",
+			Argument:  "aws-access-key-id",
+			Shorthand: "k",
+			Default:   "",
+			Usage:     "The AWS access key id to authenticate",
+			Value:     &config.AwsAccessKeyId,
 		},
+		{
+			Path:      "aws-secret-key",
+			Env:       "AWS_SECRET_KEY",
+			Argument:  "aws-secret-key",
+			Shorthand: "s",
+			Default:   "",
+			Usage:     "The AWS secret key id to authenticate",
+			Value:     &config.AwsSecretKey,
+		},
+		{
+			Path:      "aws-region",
+			Env:       "AWS_REGION",
+			Argument:  "aws-region",
+			Shorthand: "r",
+			Default:   "us-east-1",
+			Usage:     "The AWS region",
+			Value:     &config.AwsRegion,
+		},
+		{
+			Path:      "aws-allowed-instance-states",
+			Env:       "AWS_ALLOWED_INSTANCE_STATES",
+			Argument:  "aws-allowed-instance-states",
+			Shorthand: "S",
+			Default:   "running",
+			Usage:     "The EC2 instance states allowed",
+			Value:     &config.AllowedInstanceStates,
+		},
+		{
+			Path:      "aws-accounts",
+			Env:       "AWS_ACCOUNTS",
+			Argument:  "aws-accounts",
+			Shorthand: "a",
+			Default:   "",
+			Usage:     "The AWS accounts",
+			Value:     &config.AwsAccounts,
+		},
+		{
+			Path:      "timeout",
+			Env:       "TIMEOUT",
+			Argument:  "timeout",
+			Shorthand: "t",
+			Default:   uint64(10),
+			Usage:     "The plugin timeout",
+			Value:     &config.Timeout,
+		},
+	}
+
+	validInstanceStates = map[string]bool{
+		"pending":       true,
+		"running":       true,
+		"stopping":      true,
+		"stopped":       true,
+		"shutting-down": true,
+		"terminated":    true,
 	}
 )
 
@@ -47,8 +111,42 @@ func main() {
 // checkArgs is invoked by the go handler to perform validation of the values. If an error is returned
 // the handler will not be executed.
 func checkArgs(_ *types.Event) error {
-	if len(config.param1) == 0 {
-		return fmt.Errorf("param1 must contain a value")
+	if len(config.AwsRegion) == 0 {
+		return fmt.Errorf("aws-region must contain a value")
+	}
+	if len(config.AwsAccessKeyId) == 0 {
+		return fmt.Errorf("aws-access-key-id must contain a value")
+	}
+	if len(config.AwsSecretKey) == 0 {
+		return fmt.Errorf("aws-secret-key must contain a value")
+	}
+	if len(config.AwsAccounts) == 0 {
+		return fmt.Errorf("aws-accounts must contain at least one value")
+	}
+	if len(config.AllowedInstanceStates) == 0 {
+		return fmt.Errorf("allowed-instance-states must contain at least one value")
+	}
+
+	// parse the aws accounts
+	config.AwsAccountsMap = make(map[string]bool)
+	for _, account := range strings.Split(config.AwsAccounts, ",") {
+		trimmedAccount := strings.TrimSpace(account)
+		if len(trimmedAccount) > 0 {
+			config.AwsAccountsMap[trimmedAccount] = true
+		}
+	}
+
+	// parse the instance states
+	config.AllowedInstanceStatesMap = make(map[string]bool)
+	for _, instanceState := range strings.Split(config.AllowedInstanceStates, ",") {
+		trimmedInstanceState := strings.TrimSpace(instanceState)
+		if len(trimmedInstanceState) == 0 {
+			// Ignore this one
+		} else if validInstanceStates[trimmedInstanceState] {
+			config.AllowedInstanceStatesMap[trimmedInstanceState] = true
+		} else {
+			return fmt.Errorf("invalid instance state: %s", trimmedInstanceState)
+		}
 	}
 
 	return nil
@@ -56,9 +154,10 @@ func checkArgs(_ *types.Event) error {
 
 // executeHandler is executed by the go handler and executes the handler business logic.
 func executeHandler(event *types.Event) error {
-	log.Printf("param1: %s\n", config.param1)
-	jsonBytes, _ := json.Marshal(event)
-	log.Printf("Event:%s\n", string(jsonBytes))
+	configJsonBytes, _ := json.MarshalIndent(config, "", "    ")
+	log.Printf("Config:\n%s\n", string(configJsonBytes))
+	jsonBytes, _ := json.MarshalIndent(event, "", "    ")
+	log.Printf("Event:\n%s\n", string(jsonBytes))
 
 	return nil
 }
