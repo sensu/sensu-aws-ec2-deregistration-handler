ca11bd62aede5e2e5c38aa6b8b63883f8f634076
Francis Guimond <francisguimond@gmail.com>
2019-04-23 20:36:09 -0600

    Refactor to use split sensu-plugins-go-library dependency

--- aws/aws.go
+++ aws/aws.go
@@ -6,7 +6,7 @@ import (
 	"github.com/aws/aws-sdk-go/aws/credentials"
 	"github.com/aws/aws-sdk-go/aws/session"
 	"github.com/aws/aws-sdk-go/service/ec2"
-	"github.com/sensu/sensu-enterprise-go-plugin/sensu"
+	"github.com/sensu/sensu-plugins-go-library/sensu"
 	"log"
 )
 
